5ccc30a36ce6a3e67a26aa5db0162b76655f111e
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-21 10:30:23 +0800

    feat: Add instance variables, constructor, and getter & setter methods

---
+++ src/main/java/com/audreynanual/ATM.java
@@ -0,0 +1,45 @@
+package com.audreynanual;
+
+
+public class ATM {
+
+    // instance variables
+    private double balance;
+    private double amountToDeposit;
+    private double amountToWithdraw;
+
+    // default constructor 
+    public ATM() {
+
+    }
+
+    // getter and setter methods for instance variables
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAmountToDeposit() {
+        return amountToDeposit;
+    }
+
+    public void setAmountToDeposit(double amountToDeposit) {
+        this.amountToDeposit = amountToDeposit;
+    }
+
+    public double getAmountToWithdraw() {
+        return amountToWithdraw;
+    }
+
+    public void setAmountToWithdraw(double amountToWithdraw) {
+        this.amountToWithdraw = amountToWithdraw;
+    }
+
+    // getter and setter methods
+    
+
+
+}
