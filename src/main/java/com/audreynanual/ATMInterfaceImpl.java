5cd9aaf2c8a71d612fd72864d259edf9d92d0024
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-21 13:05:39 +0800

    refactor: Rename ATMInterfaceImplementation to ATMInterfaceImp

---
+++ src/main/java/com/audreynanual/ATMInterfaceImpl.java
@@ -0,0 +1,28 @@
+package com.audreynanual;
+
+public class ATMInterfaceImpl implements ATMInterface{
+
+    @Override
+    public void viewBalance() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void withdrawAmount(double amountToWithdraw) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void depositAmount(double amountToDeposit) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewAccountStatement() {
+        // TODO Auto-generated method stub
+        
+    }
+}
