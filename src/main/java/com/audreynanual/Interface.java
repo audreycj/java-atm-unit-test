c7e02edf07b08993e549c7319fef52ccef1aac31
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-21 11:28:38 +0800

    refactor: Rename interface name

--- src/main/java/com/audreynanual/Interface.java
+++
@@ -1,8 +0,0 @@
-package com.audreynanual;
-
-public interface Interface {
-    public void viewBalance();
-    public void withdrawAmount(double amountToWithdraw);
-    public void depositAmount(double amountToDeposit);
-    public void viewAccountStatement();
-}
