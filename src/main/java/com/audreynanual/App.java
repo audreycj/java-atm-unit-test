7b18251369a2e4fc99ab8347de1dfcc45d804a55
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-21 10:45:24 +0800

    feat: Add user validation via ATM account and PIN

--- src/main/java/com/audreynanual/App.java
+++ src/main/java/com/audreynanual/App.java
@@ -1,13 +1,41 @@
 package com.audreynanual;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // correct ATM account and PIN values
+        int atmAccount = 12345;
+        int atmPIN = 123;
+
+        // welcome message
+        System.out.println("\nWelcome!\n");
+
+        // asking user input for ATM account and PIN
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter ATM number: ");
+        int inputAccount = userInput.nextInt();
+
+        System.out.print("Enter PIN: ");
+        int inputPIN = userInput.nextInt();
+
+        // validating user number and PIN
+        if ((atmAccount == inputAccount) && (atmPIN == inputPIN)) {
+            System.out.println("\nValidation successful.\n");
+
+            // displaying the ATM menu of services
+            System.out.println("1. View available balance");
+            System.out.println("2. Withdraw amount");
+            System.out.println("3. Deposit amount");
+            System.out.println("4. View account statement");
+            System.out.println("5. Exit");
+
+            // 
+
+        } else {
+            System.out.println("\nInvalid ATM account or PIN.\n");
+            System.exit(0);
+        }
     }
 }
