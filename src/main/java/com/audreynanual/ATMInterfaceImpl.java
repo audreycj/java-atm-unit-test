0cb5763e768c9fa31c92d3e26845f66e04cf81bc
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-21 15:39:52 +0800

    docs: Add comments

--- src/main/java/com/audreynanual/ATMInterfaceImpl.java
+++ src/main/java/com/audreynanual/ATMInterfaceImpl.java
@@ -9,17 +9,23 @@ public class ATMInterfaceImpl implements ATMInterface{
     // HashMap for account statement
     HashMap<Double, String> statement = new HashMap<>();
 
+    // viewBalance() function - displays the current amount of funds in the ATM account
     @Override
     public void viewBalance() {
         System.out.print("Available balance: PHP " + atm.getBalance() + "\n\n");
     }
 
+    // withdrawAmount() function - withdraws the amount inputted by the user from the account's current balance
     @Override
     public void withdrawAmount(double amountToWithdraw) {
+
+        // if the inputted amount is 0 or lesser than the account's current balance, the withdraw operation is denied
         if (amountToWithdraw > atm.getBalance()) {
             System.out.println("\nInsufficient balance.\n");
+        
         } else if (amountToWithdraw == 0) {
             System.out.println("\nInvalid value.\n");
+
         } else {
             atm.setBalance(atm.getBalance() - amountToWithdraw);
             System.out.println("PHP " + amountToWithdraw + " withrawn successfully.\n");
@@ -27,8 +33,11 @@ public class ATMInterfaceImpl implements ATMInterface{
         }
     }
 
+    // depositeAmount() function - deposits the amount inputted by the user into the account's current balance
     @Override
     public void depositAmount(double amountToDeposit) {
+
+        // if the inputted amount is 0 or a negetive value, the deposit operation is denied
         if (amountToDeposit < 1) {
             System.out.println("\nInvalid value.\n");
         } else {
@@ -38,9 +47,10 @@ public class ATMInterfaceImpl implements ATMInterface{
         }
     }
 
+    // viewAccountStatement() function - displays
     @Override
     public void viewAccountStatement() {
-        // TODO Auto-generated method stub
+        
         
     }
 }
