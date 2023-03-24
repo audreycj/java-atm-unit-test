53e913424153886619c1d3872d538050549a4eea
Audrey Carmel Jay J. Nanual <68633976+audreycj@users.noreply.github.com>
2023-03-24 14:28:42 +0800

    feat: Add tests related to amountToWithdrawn()

--- src/test/java/com/audreynanual/ATMInterfaceImplTest.java
+++ src/test/java/com/audreynanual/ATMInterfaceImplTest.java
@@ -5,4 +5,34 @@ import static org.junit.Assert.*;
 
 public class ATMInterfaceImplTest {
 
+    // unit tests for the withdrawAmount() function
+    @Nested
+    @DisplayName("Unit tests for the withdrawAmount() function")
+    class WithdrawTest {
+
+        // test if amountToWithdraw is more than the current balance
+        @Test
+        public void testWithdrawAmount_IfAmountIsMoreThanBalance() {
+            ATMInterfaceImpl atmInterfaceImpl = new ATMInterfaceImpl();
+            atmInterfaceImpl.withdrawAmount(1000);
+            assertEquals("Insufficient balance.", "Insufficient balance.");
+        }
+
+        // test if amountToWithdraw is 0
+        @Test
+        public void testWithdrawAmount_IfAmountIsZero() {
+            ATMInterfaceImpl atmInterfaceImpl = new ATMInterfaceImpl();
+            atmInterfaceImpl.withdrawAmount(0);
+            assertEquals("Invalid value.", "Invalid value.");
+        }
+
+        // test if amountToWithdraw is divisible by 20 or 50
+        @Test
+        public void testWithdrawAmount_IfAmountIsNotDivisibleBy20Or50() {
+            ATMInterfaceImpl atmInterfaceImpl = new ATMInterfaceImpl();
+            atmInterfaceImpl.withdrawAmount(25);
+            assertEquals("The ATM can only give out banknotes.", "The ATM can only give out banknotes.");
+        }
+
+    }
 }
